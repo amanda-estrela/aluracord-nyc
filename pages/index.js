6e16b645910e5477f6391be89f98211e26281dbf
Amanda Estrela <efernandes.amanda@gmail.com>
2022-01-30 23:29:11 -0300

    Página de chat

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,7 @@
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
-import React, { useState } from 'react';
+import React from 'react';
+import {useRouter} from 'next/router';
 import appConfig from "../config.json"
-
-function GlobalStyle() {
-    return (
-        <style global jsx>{`
-        * {
-          margin: 0;
-          padding: 0;
-          box-sizing: border-box;
-          list-style: none;
-        }
-        body {
-          font-family: 'Open Sans', sans-serif;
-        }
-        /* App fit Height */ 
-        html, body, #__next {
-          min-height: 100vh;
-          display: flex;
-          flex: 1;
-        }
-        #__next {
-          flex: 1;
-        }
-        #__next > * {
-          flex: 1;
-        }
-        /* ./App fit Height */ 
-      `}</style>
-    );
-}
 
 function Titulo(props) {
     const Tag = props.tag || 'h1';
@@ -62,11 +34,10 @@ function Titulo(props) {
 export default function PaginaInicial() {
    // const username = 'amanda-estrela';
    const [username, setUsername] = React.useState('amanda-estrela');
-   
+   const roteamento = useRouter();
 
     return (
         <>
-            <GlobalStyle />
             <Box
                 styleSheet={{
                     display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -93,6 +64,11 @@ export default function PaginaInicial() {
                     {/* Formulário */}
                     <Box
                         as="form"
+                        onSubmit={function(infosDoEvento){
+                            infosDoEvento.preventDefault();
+                            roteamento.push('/chat');
+                        }
+                    }
                         styleSheet={{
                             display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
                             width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -120,7 +96,7 @@ export default function PaginaInicial() {
                         <TextField
                             value={username}
                             onChange={ function(event){
-                                console.log('usuário dgitou, event.target.value');
+                                console.log('usuário dgitou', event.target.value);
                                 //onde está o valor?
                                 const valor = event.target.value;
                                 //trocar o valor da vaiável
